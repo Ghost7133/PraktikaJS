4ec5c9641bc31ab5a87691e65196225a1302fb12
Ghost7133 <118304258+Ghost7133@users.noreply.github.com>
2023-04-04 11:52:59 +0300

    Update script.js

--- 1/script.js
+++ 1/script.js
@@ -1,266 +1,334 @@
-(function(){
-//██████████████████████████████████████████████//
-//█────█───█────█─██─█────█────█────██─██─█────█//
-//█─██─██─██─██─█─██─█─██─█─██─█─██──█─█─██─██─█//
-//█─██─██─██────█─█──█─████─██─█────██──███────█//
-//█─██─██─██─████──█─█─██─█─██─█─██──█─█─██─██─█//
-//█────██─██─████─██─█────█────█────██─██─█─██─█//
-//██████████████████████████████████████████████//
-    const massiveWork = [];
-    const massiveWorkCheck = [];
-    //Некоторые элементы для отрисовки
+(function () {
+  //██████████████████████████████████████████████//
+  //█────█───█────█─██─█────█────█────██─██─█────█//
+  //█─██─██─██─██─█─██─█─██─█─██─█─██──█─█─██─██─█//
+  //█─██─██─██────█─█──█─████─██─█────██──███────█//
+  //█─██─██─██─████──█─█─██─█─██─█─██──█─█─██─██─█//
+  //█────██─██─████─██─█────█────█────██─██─█─██─█//
+  //██████████████████████████████████████████████//
+  const massiveWork = [];
+  const massiveWorkCheck = [];
+  let idMassiva = 0;
+  let indexDela = 0;
+  //Некоторые элементы для отрисовки
 
-        //Контейнер для всех дел//
-        function allDivTextRend() {
-            allDivText.style.backgroundColor = `#4F4F4F`
-            allDivText.style.color = `white`
-        }
+  //Контейнер для всех дел//
+  function allDivTextRend() {
+    allDivText.style.backgroundColor = `#4F4F4F`;
+    allDivText.style.color = `white`;
+  }
 
+  //Основной текст//
+  function h1TextRend(title) {
+    let h1Text = document.createElement(`h1`);
+    h1Text.textContent = title;
+    return h1Text;
+  }
 
-        //Основной текст//
-        function h1TextRend(title) {
-            let h1Text = document.createElement(`h1`)
-            h1Text.textContent = title
-            return h1Text
-        }
-        
-        
-    //Общее поле ввода//
+  //Общее поле ввода//
 
-        //Текст//
+  //Текст//
 
-            //Поле ввода дел//
-        function inpTextRend(){
-            let inpText = document.createElement(`h3`)
-            inpText.textContent = `Дело`
-            inpText.style.width = `208px`
-            inpText.style.margin = `0`
-            inpText.style.float = `left`
-            inpText.style.textAlign = `center`
-            return inpText
-        }
+  //Поле ввода дел//
+  function inpTextRend() {
+    let inpText = document.createElement(`h3`);
+    inpText.textContent = `Дело`;
+    inpText.style.width = `208px`;
+    inpText.style.margin = `0`;
+    inpText.style.float = `left`;
+    inpText.style.textAlign = `center`;
+    return inpText;
+  }
 
-        
-            //Поле ввода часа//
-        function hourTextRend(){
-            let hourText = document.createElement(`h3`)
-            hourText.textContent = `Часы`
-            hourText.style.width = `177px`
-            hourText.style.margin = `0`
-            hourText.style.float = `left`
-            hourText.style.textAlign = `center`
-            return hourText
-        }
+  //Поле ввода часа//
+  function hourTextRend() {
+    let hourText = document.createElement(`h3`);
+    hourText.textContent = `Часы`;
+    hourText.style.width = `177px`;
+    hourText.style.margin = `0`;
+    hourText.style.float = `left`;
+    hourText.style.textAlign = `center`;
+    return hourText;
+  }
 
-            //Поле ввода минут
-        function minuteTextRend(){
-            let minuteText = document.createElement(`h3`)
-            minuteText.textContent = `Минуты`
-            minuteText.style.width = `177px`
-            minuteText.style.margin = `0`
-            minuteText.style.float = `left`
-            minuteText.style.textAlign = `center`
-            return minuteText
-        }
-        
-            //Поле ввода секунд
-        function secondsTextRend(){
-            let secondsText = document.createElement(`h3`)
-            secondsText.textContent = `Секунды`
-            secondsText.style.textAlign = `left`
-            return secondsText
-        }
+  //Поле ввода минут
+  function minuteTextRend() {
+    let minuteText = document.createElement(`h3`);
+    minuteText.textContent = `Минуты`;
+    minuteText.style.width = `177px`;
+    minuteText.style.margin = `0`;
+    minuteText.style.float = `left`;
+    minuteText.style.textAlign = `center`;
+    return minuteText;
+  }
 
+  //Поле ввода секунд
+  function secondsTextRend() {
+    let secondsText = document.createElement(`h3`);
+    secondsText.textContent = `Секунды`;
+    secondsText.style.textAlign = `left`;
+    return secondsText;
+  }
 
-        //Ввод//
+  //Ввод//
 
-            //Поле ввода дел//
-        function inpRend(){
-            let inp = document.createElement(`input`)
-            inp.setAttribute(`placeholder`,`Введите дело`)
-            inp.style.width = `200px`
-            inp.style.textAlign = `left`
-            console.log(inp.value);
-            console.log(`123`);
-            return inp
-        }
+  //Поле ввода дел//
+  function inpRend() {
+    let inp = document.createElement(`input`);
+    inp.setAttribute(`placeholder`, `Введите дело`);
+    inp.style.width = `200px`;
+    inp.style.textAlign = `left`;
+    return inp;
+  }
 
-            //Поле ввода часа//
-        function inpHourRend(){
-            let inpHour = document.createElement(`input`)
-            inpHour.value = `0`
-            inpHour.setAttribute(`placeholder`,`0`)
-            return inpHour
-        }
+  //Поле ввода часа//
+  function inpHourRend() {
+    let inpHour = document.createElement(`input`);
+    inpHour.value = `0`;
+    inpHour.setAttribute(`placeholder`, `0`);
+    return inpHour;
+  }
 
-            //Поле ввода минут
-        function inpMinuteRend(){
-            let inpMinute = document.createElement(`input`)
-            inpMinute.value = `0`
-            inpMinute.setAttribute(`placeholder`,`0`)
-            return inpMinute
-        }
-            //Поле ввода секунд
-        function inpSecondRend(){
-            let inpSecond = document.createElement(`input`)
-            inpSecond.value = `0`
-            inpSecond.setAttribute(`placeholder`,`0`)
-            return inpSecond
-        }
+  //Поле ввода минут
+  function inpMinuteRend() {
+    let inpMinute = document.createElement(`input`);
+    inpMinute.value = `0`;
+    inpMinute.setAttribute(`placeholder`, `0`);
+    return inpMinute;
+  }
+  //Поле ввода секунд
+  function inpSecondRend() {
+    let inpSecond = document.createElement(`input`);
+    inpSecond.value = `0`;
+    inpSecond.setAttribute(`placeholder`, `0`);
+    return inpSecond;
+  }
 
+  //Кнопка//
+  function buttonRend() {
+    let btn = document.createElement(`button`);
+    allDivText.append(btn);
+    btn.setAttribute(`id`, `btn`);
+    btn.disabled = true;
+    btn.textContent = `Добавить`;
+    return btn;
+  }
 
-        //Кнопка//
-        function buttonRend(){
-            let btn = document.createElement(`button`);
-            allDivText.append(btn);
-            btn.setAttribute(`id`, `btn`);
-            btn.disabled = true 
-            btn.textContent = `Добавить`;
-            return btn
-        }
-    
-    document.addEventListener('DOMContentLoaded', function (){
-        let container = document.getElementById('allDivText');
+  document.addEventListener("DOMContentLoaded", function () {
+    let container = document.getElementById("allDivText");
 
-        let h1TextRendering = h1TextRend('Список дел');
+    let h1TextRendering = h1TextRend("Список дел");
 
-        let inpTextRendering = inpTextRend()
-        let hourTextRendering = hourTextRend()
-        let minuteTextRendering = minuteTextRend()
-        let secondsTextRendering = secondsTextRend()
+    let inpTextRendering = inpTextRend();
+    let hourTextRendering = hourTextRend();
+    let minuteTextRendering = minuteTextRend();
+    let secondsTextRendering = secondsTextRend();
 
-        let inpRendering = inpRend()
-        let inpHourRendering = inpHourRend()
-        let inpMinuteRendering = inpMinuteRend()
-        let inpSecondRendering = inpSecondRend()
+    let inpRendering = inpRend();
+    let inpHourRendering = inpHourRend();
+    let inpMinuteRendering = inpMinuteRend();
+    let inpSecondRendering = inpSecondRend();
 
-        let buttonRendering = buttonRend()
+    let buttonRendering = buttonRend();
 
+    allDivTextRend();
 
-        
-        allDivTextRend()
+    //Контейнер для дел//
 
-        container.append(h1TextRendering)
-
-        container.append(inpTextRendering)        
-        container.append(hourTextRendering)
-        container.append(minuteTextRendering)
-        container.append(secondsTextRendering)
-
-        container.append(inpRendering)
-        container.append(inpHourRendering)
-        container.append(inpMinuteRendering)
-        container.append(inpSecondRendering)
-
-        container.append(buttonRendering)
-
-//██████████████████████████████████████████████████████████████//
-//█─────█─█─█─██─█─██─█─██─█─██─█─██─██─██─█────█─██─█────███──█//
-//█─█─█─█─█─█─█─██─██─█─█─██─██─█─██─██─██─█─██─█─██─█─██─██─█─█//
-//█─█─█─█───█──███────█──███─█──█─██─██─█──█─██─█────█────█─██─█//
-//█─────███─█─█─██─██─█─█─██──█─█─██─██──█─█─██─█─██─█─██─█─██─█//
-//███─███───█─██─█─██─█─██─█─██─█─────█─██─█────█─██─█─██─█─██─█//
-//██████████████████████████████████████████████████████████████//
-
-function check(){
-    if(inpRendering.value.length != 0){
-        buttonRendering.disabled = false}
-    else buttonRendering.disabled = true
+    function divTextRend() {
+      let divText = document.createElement(`div`);
+      return divText;
     }
-setInterval(check, 1); 
-//Функция создания дел//
 
+    function textRend() {
+      let text = document.createElement(`p`);
+      text.textContent =
+        inpRendering.value +
+        " " +
+        ` after ${inpHourRendering.value} Hour ${inpMinuteRendering.value} Minute ${inpSecondRendering.value} seconds`;
+      inpRendering.value = ``;
+      return text;
+    }
 
-document.querySelector(`#btn`);
-btn.addEventListener(`click`, function(){
-    //Проверка//
-    if (inpRendering.value.length > 0){
-        if (!massiveWork.includes(inpRendering.value)){
-            massiveWork.push(inpRendering.value,)
-            massiveWorkCheck.push({
-                name: inpRendering.value,
-                status: "false"
-                })
-            console.log(massiveWork);
-            console.log(massiveWorkCheck);
- 
-            //Отрисовка элементов
+    function confirmButtonRend() {
+      let confirmButton = document.createElement(`button`);
+      confirmButton.textContent = `✔`;
+      confirmButton.setAttribute(`id`, `confirmButton`);
+      return confirmButton;
+    }
 
-                //Контейнер для дел//
-                let divText = document.createElement(`div`)
-                container.append(divText)
+    function cancelButtonRend() {
+      let cancelButton = document.createElement(`button`);
+      cancelButton.setAttribute(`id`, `cancelButton`);
+      cancelButton.textContent = `×`;
+      return cancelButton;
+    }
+    function restartButtonRend() {
+      let restartButton = document.createElement(`button`);
+      restartButton.setAttribute(`id`, `cancelButton`);
+      restartButton.textContent = `Выгрузить`;
+      return restartButton;
+    }
 
-                //Текст дела//
-                let text = document.createElement(`p`)
-                divText.append(text)
-                text.textContent = inpRendering.value + " "+ ` after ${inpHourRendering.value} Hour ${inpMinuteRendering.value} Minute ${inpSecondRendering.value} seconds`
-                let _textContent = inpRendering.value
-                //Время на исполнение
-                let time = (parseInt(inpHourRendering.value)*3600)+(parseInt(inpMinuteRendering.value)*60)+(parseInt(inpSecondRendering.value))
+    function confirms() {
+      textRendering.style.textDecoration = `line-through`;
+      textRendering.style.color = `green`;
+      textRendering.style.backgroundColor = `blacks`;
+      massiveWorkCheck[massiveWork.indexOf(_textContent)].status = true;
+      textRendering.setAttribute(`class`, `true`);
 
-                const func = () => {
-                    alert(`${inpRendering.value} время вышло`);
-                    text.style.color = `red`
-                  };
-                  setTimeout(func, time * 1000);
+      localStorage.setItem(`statusDelov`, JSON.stringify(massiveWorkCheck));
+      console.log(JSON.parse(localStorage.getItem(`statusDelov`)));
 
-                //Кнопка выполнить//
-                let confirmButton = document.createElement(`button`)
-                confirmButton.textContent = `✔`;
-                confirmButton.setAttribute(`id`,`confirmButton`)
-                divText.append(confirmButton)
-                
+      console.log(`Выполнено`);
+    }
 
-                //Кнопка удалить//
-                let cancelButton = document.createElement(`button`)
-                cancelButton.setAttribute(`id`,`cancelButton`)
-                cancelButton.textContent = `×`;
-                divText.append(cancelButton)
-                
-            //Функционал//
+    function cancel() {
+      if (confirm("Вы уверены что хотите удалить дело ?")) {
+        divTextRendering.removeChild(textRendering);
+        divTextRendering.removeChild(confirmButtonRendering);
+        divTextRendering.removeChild(cancelButtonRendering);
+        massiveWork.splice(massiveWork.indexOf(_textContent), 1);
 
-                //Функция подтвердить//
-                function confirms(){
-                console.log(`Выполнено`);
-                text.style.textDecoration = `line-through`
-                text.style.color = `green`
-                text.style.backgroundColor = `blacks`
-                massiveWorkCheck[massiveWork.indexOf(_textContent)].status = true
-                console.log(massiveWork);
-                console.log(massiveWorkCheck);
-                text.setAttribute(`class`,`gone`)
-                console.log(text.getAttribute(`class`));
-                }
-
-                //Функция отменить//
-                function cancel(){
-                    console.log(`Удалить`);
-                    if (confirm("Вы уверены что хотите удалить дело ?")){
-                        divText.removeChild(text)
-                        divText.removeChild(confirmButton)
-                        divText.removeChild(cancelButton)
-                        massiveWork.splice(massiveWork.indexOf(_textContent),1) 
-                        console.log(massiveWork);
-                    }
-                }
-
-                document.querySelector(`#confirmbutton`);
-                confirmButton.addEventListener(`click`, confirms);
-
-                document.querySelector(`#cancelButton`);
-                cancelButton.addEventListener(`click`, cancel);       
+        for (let i = 0; i < massiveWorkCheck.length; i++) {
+          if (massiveWorkCheck[i].name == _textContent) {
+            indexDela = i;
+            break;
+          }
         }
 
-        else alert(`Такое дело уже есть`)
-    }
-    else{
-        alert(`Вы ничего не ввели`)
-    }
-    if (inpRendering.value.length != 0) {
-        buttonRendering.disabled = false
-    }
-   })
-  })
-  
-})()
+        massiveWorkCheck.splice(indexDela, 1);
 
+        localStorage.setItem(`statusDelov`, JSON.stringify(massiveWorkCheck));
+        console.log(JSON.parse(localStorage.getItem(`statusDelov`)));
+
+        console.log(`Удалить`);
+      }
+    }
+
+    let restartButtonRendering = restartButtonRend();
+
+    let _textContent = inpRendering.value;
+
+    let divTextRendering = divTextRend();
+    let textRendering = textRend();
+    let confirmButtonRendering = confirmButtonRend();
+    let cancelButtonRendering = cancelButtonRend();
+
+    container.append(h1TextRendering);
+
+    container.append(inpTextRendering);
+    container.append(hourTextRendering);
+    container.append(minuteTextRendering);
+    container.append(secondsTextRendering);
+
+    container.append(inpRendering);
+    container.append(inpHourRendering);
+    container.append(inpMinuteRendering);
+    container.append(inpSecondRendering);
+
+    container.append(buttonRendering);
+    container.append(restartButtonRendering);
+
+    //██████████████████████████████████████████████████████████████//
+    //█─────█─█─█─██─█─██─█─██─█─██─█─██─██─██─█────█─██─█────███──█//
+    //█─█─█─█─█─█─█─██─██─█─█─██─██─█─██─██─██─█─██─█─██─█─██─██─█─█//
+    //█─█─█─█───█──███────█──███─█──█─██─██─█──█─██─█────█────█─██─█//
+    //█─────███─█─█─██─██─█─█─██──█─█─██─██──█─█─██─█─██─█─██─█─██─█//
+    //███─███───█─██─█─██─█─██─█─██─█─────█─██─█────█─██─█─██─█─██─█//
+    //██████████████████████████████████████████████████████████████//
+
+    //отрисовка после перезагрузки//
+    //window.addEventListener(`unload`, localStorage.clear);
+    ////
+
+    function checkRestart() {
+      if (
+        localStorage.getItem("statuseImen") != null ||
+        localStorage.getItem("statusDelov") != null
+      ) {
+        restartButtonRendering.disabled = false;
+      } else restartButtonRendering.disabled = true;
+    }
+    setInterval(checkRestart, 1);
+
+    restartButtonRendering.addEventListener(`click`, function () {
+      if (
+        localStorage.getItem("statuseImen") != null ||
+        localStorage.getItem("statusDelov") != null
+      ) {
+        (massiveWork.length = 0),
+          (massiveWorkCheck.length = 0),
+          massiveWork.push(JSON.parse(localStorage.getItem(`statusImen`))),
+          massiveWorkCheck.push(
+            JSON.parse(localStorage.getItem(`statusDelov`))
+          ),
+          container.append(divTextRendering),
+          divTextRendering.append(textRendering),
+          divTextRendering.append(confirmButtonRendering),
+          divTextRendering.append(cancelButtonRendering);
+      }
+    });
+
+    /////////
+    function check() {
+      if (inpRendering.value.length != 0) {
+        buttonRendering.disabled = false;
+      } else buttonRendering.disabled = true;
+    }
+    setInterval(check, 1);
+
+    inpRendering.addEventListener("keyup", function (event) {
+      if (event.key === "Enter") {
+        buttonRendering.click();
+      }
+    });
+
+    //Функция создания дел//
+
+    buttonRendering.addEventListener(`click`, function () {
+      //Проверка//
+      if (!massiveWork.includes(inpRendering.value)) {
+        massiveWork.push(inpRendering.value);
+        massiveWorkCheck.push({
+          name: inpRendering.value,
+          status: "false",
+        });
+        localStorage.setItem(`statusImen`, JSON.stringify(massiveWorkCheck));
+
+        localStorage.setItem(`statusDelov`, JSON.stringify(massiveWorkCheck));
+        console.log(JSON.parse(localStorage.getItem(`statusDelov`)));
+
+        //Отрисовка элементов//
+
+        container.append(divTextRendering);
+        divTextRendering.append(textRendering);
+
+        let time =
+          parseInt(inpHourRendering.value) * 3600 +
+          parseInt(inpMinuteRendering.value) * 60 +
+          parseInt(inpSecondRendering.value);
+
+        const func = () => {
+          alert(`${inpRendering.value} время вышло`);
+          textRendering.style.color = `red`;
+        };
+        setTimeout(func, time * 1000);
+
+        divTextRendering.append(confirmButtonRendering);
+        divTextRendering.append(cancelButtonRendering);
+
+        //Функционал//
+
+        document.querySelector(`#confirmbutton`);
+        confirmButtonRendering.addEventListener(`click`, confirms);
+
+        document.querySelector(`#cancelButton`);
+        cancelButtonRendering.addEventListener(`click`, cancel);
+      } else alert(`Такое дело уже есть`);
+      if (inpRendering.value.length != 0) {
+        buttonRendering.disabled = false;
+      }
+    });
+  });
+})();
